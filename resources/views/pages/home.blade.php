494531151292cec4ef347e44d5e07370acae9abc
Cperry <codyperry65@yahoo.com>
2018-03-08 23:23:53 -0500

    Add header-content and styling

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,5 +9,11 @@
     </div>
     <div class="header-img">
     </div>
+    <div class="header-content">
+      <h1>Custom Digital Solutions.</h1>
+      <p>Perry's' Development is an innovation forward-thinking web development firm. 
+      Let us build the technology specific to your companies wants and needs.</p>
+      <a href="#">Get Started!</a>
+    </div>
   </header>
 @endsection

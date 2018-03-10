ff16e9e644801f527e682f98bee63b30f47bc03c
Cperry <codyperry65@yahoo.com>
2018-03-10 14:32:43 -0500

    Add nav bar and logo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,25 +3,41 @@
 
 @section('main')
   <header id="site-header">
-    <div class="contact-header">
-      <span class="social-icons">logos</span>
-      <span class="message-us">Contact Us!</span>
+  <div class="contact-header">
+    <span class="social-icons">Icons</span>
+    <span class="message-us">Contact Us!</span>
+  </div>
+
+<nav class="navbar navbar-expand-md navbar-dark">
+    <img class="logo" src="/images/pd-logo.svg" alt="Perry's Development Logo">
+    <a href="/" class="navbar-brand">Perry's Development</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Our Story</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Work</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Services</a>
+            </li>
+        </ul>
     </div>
-    <nav class="header-nav">
-      <ul>
-        <li>Home</li>
-        <li>Our Story</li>
-          <!-- TODO: insert logo -->
-        <li><span class="logo"></span></li>
-        <li>Work</li>
-        <li>Services</li>
-      </ul>
-    </nav>
+</nav>
+
+
+
+
+
     <div class="header-img">
     </div>
     <div class="header-content">
       <h1>Custom Digital Solutions.</h1>
-      <p>Perry's' Development is an innovative forward-thinking web development firm. 
+      <p>Perry's Development is an innovative forward-thinking web development firm. 
       We build digital platforms for businesses of all sizes.</p>
       <a href="#">Get Started!</a>
     </div>
@@ -32,7 +48,4 @@
   <div class="services-offered">
     <span class="divider">What we offer</span>
   </div>
-
-
-
 @endsection

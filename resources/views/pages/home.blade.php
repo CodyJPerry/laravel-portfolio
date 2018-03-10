319c2c17038ec88b08a8dfacaf66b2935ed51075
Cperry <codyperry65@yahoo.com>
2018-03-10 16:01:32 -0500

    Branch header section into partial

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,50 +2,42 @@
 
 
 @section('main')
-  <header id="site-header">
-  <div class="contact-header">
-    <span class="social-icons">Icons</span>
-    <span class="message-us">Contact Us!</span>
-  </div>
-
-<nav class="navbar navbar-expand-md navbar-dark">
-    <img class="logo" src="/images/pd-logo.svg" alt="Perry's Development Logo">
-    <a href="/" class="navbar-brand">Perry's Development</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Our Story</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Work</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Services</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-
-
-
-
-    <div class="header-img">
-    </div>
-    <div class="header-content">
-      <h1>Custom Digital Solutions.</h1>
-      <p>Perry's Development is an innovative forward-thinking web development firm. 
-      We build digital platforms for businesses of all sizes.</p>
-      <a href="#">Get Started!</a>
-    </div>
-  </header>
+  @include('partials.header')
 
   
   <!-- WHAT WE OFFER -->
   <div class="services-offered">
     <span class="divider">What we offer</span>
   </div>
+
+  <div class="container">
+  <h3>Our Process</h3>
+    <div class="row">
+      <div class="col-sm-3">
+        <img src="http://placekitten.com/g/200/200" alt="placeholder">
+        <h4>Analytics</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, recusandae.</p>
+        <a class="text-center" href="#">Button</a> 
+      </div>
+      <div class="col-sm-3">
+          <img src="http://placekitten.com/g/200/200" alt="placeholder">
+          <h4>Design</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, blanditiis.</p>
+          <a href="#">Button</a>  
+      </div>
+      <div class="col-sm-3">
+        <img src="http://placekitten.com/g/200/200" alt="placeholder">
+        <h4>Development</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, dolorem?</p>
+        <a href="#">Button</a>  
+      </div>
+      <div class="col-sm-3">
+        <img src="http://placekitten.com/g/200/200" alt="placeholder">
+        <h4>Maintenance</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, tempora!</p>
+        <a href="#">Button</a>  
+      </div>
+    </div>
+  </div>
+  
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+  <header id="site-header">
+  <div class="contact-header">
+    <span class="social-icons">Icons</span>
+    <span class="message-us">Contact Us!</span>
+  </div>
+
+<nav class="navbar navbar-expand-md navbar-dark">
+    <img class="logo" src="/images/pd-logo.svg" alt="Perry's Development Logo">
+    <a href="/" class="navbar-brand">Perry's Development</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Our Story</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Work</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Services</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+    <div class="header-img">
+    </div>
+    <div class="header-content">
+      <h1>Custom Digital Solutions.</h1>
+      <p>Perry's Development is an innovative forward-thinking web development firm. 
+      We build digital platforms for businesses of all sizes.</p>
+      <a href="#">Get Started!</a>
+    </div>
+  </header>

37f678d8eb4afa95e2ff0e3d0106ff5e7c382767
Cperry <codyperry65@yahoo.com>
2018-03-11 14:05:19 -0400

    Add button text

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
   
   <!-- WHAT WE OFFER -->
   <div class="services-offered">
-      <span class="divider">What we offer</span>
+      <span class="divider">What We Offer</span>
       <div class="container">
       <h3>Our Process <i class="far fa-check-circle"></i></h3>
         <div class="row">
@@ -15,25 +15,25 @@
             <img class="service-img" src="/images/analytics-icon.svg" alt="Analytics Icon">
             <h4 class="service-title">Analytics</h4>
             <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, recusandae.</p>
-            <a class="service-button" href="#">Button</a> 
+            <a class="service-button" href="#">Learn More!</a> 
           </div>
           <div class="col-sm-3">
               <img class="service-img" src="/images/design-icon.svg" alt="Design Icon">
               <h4 class="service-title">Design</h4>
               <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, blanditiis.</p>
-              <a class="service-button" href="#">Button</a>  
+              <a class="service-button" href="#">Learn More!</a>  
           </div>
           <div class="col-sm-3">
             <img class="service-img" src="/images/development-icon.svg" alt="Development Icon">
             <h4 class="service-title">Development</h4>
             <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, dolorem?</p>
-            <a class="service-button" href="#">Button</a>  
+            <a class="service-button" href="#">Learn More!</a>  
           </div>
           <div class="col-sm-3">
             <img class="service-img" src="/images/maintenance-icon.svg" alt="Maintenance Icon">
             <h4 class="service-title">Maintenance</h4>
             <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, tempora!</p>
-            <a class="service-button" href="#">Button</a>  
+            <a class="service-button" href="#">Learn More!</a>  
           </div>
         </div>
       </div>

a45f80f13e38f9bb6d550f3fa9e41ee651b49eab
Cperry <codyperry65@yahoo.com>
2018-03-11 22:05:57 -0400

    Add services section content

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,25 +14,25 @@
           <div class="col-sm-3">
             <img class="service-img" src="/images/analytics-icon.svg" alt="Analytics Icon">
             <h4 class="service-title">Analytics</h4>
-            <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, recusandae.</p>
+            <p class="service-text">We work with you to create an online presence that will get noticed.</p>
             <a class="service-button" href="#">Learn More!</a> 
           </div>
           <div class="col-sm-3">
               <img class="service-img" src="/images/design-icon.svg" alt="Design Icon">
               <h4 class="service-title">Design</h4>
-              <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, blanditiis.</p>
+              <p class="service-text">From Brand Identity to UI/UX design, we’ll help you engage customers.</p>
               <a class="service-button" href="#">Learn More!</a>  
           </div>
           <div class="col-sm-3">
             <img class="service-img" src="/images/development-icon.svg" alt="Development Icon">
             <h4 class="service-title">Development</h4>
-            <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, dolorem?</p>
+            <p class="service-text">Our team will create a functional solution that will exceed users expectations.</p>
             <a class="service-button" href="#">Learn More!</a>  
           </div>
           <div class="col-sm-3">
             <img class="service-img" src="/images/maintenance-icon.svg" alt="Maintenance Icon">
             <h4 class="service-title">Maintenance</h4>
-            <p class="service-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, tempora!</p>
+            <p class="service-text">Not a fan of code. No problem, we provide complete maintenance plans!</p>
             <a class="service-button" href="#">Learn More!</a>  
           </div>
         </div>
